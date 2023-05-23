21e33840b9711b98e086641bdf2ccc01c1a892e4
Jonatan Morales <johnta2610@hotmail.com>
2023-05-23 14:00:30 +0000

    ✨ S04E02 Vista NotFound

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Importando Express
 import express from 'express';
+//  Importando htpp-status
+import httpStatus from 'http-status';
 
 // Importando el enrutador
 import adminRouter from './routes/admin.route.js';
 import shopRouter from './routes/shop.route.js';
+import notFound from './routes/notFound.route.js';
 
 // Creando la instancia de express
 // que basicamente es un middleware
@@ -11,11 +14,12 @@ const app = express();
 
 // Se registra el middleware del body-parser
 app.use(express.urlencoded({ extended: true }));
-
 // Se agrega ruta de administrador
 app.use(adminRouter);
 // Se agrega ruta shop
 app.use(shopRouter);
+//  Registrando middleware para error 404
+app.use(notFound);
 
 
 // Definiendo puertos
---
+++ routes/notFound.route.js
@@ -0,0 +1,13 @@
+// Importando el enrutador de express
+import { Router } from 'express';
+//  Importando el gestor de rutas
+import path from 'path';
+
+// Creando una instancia del enrutador de express
+const router = Router();
+
+router.use((req, res) => {
+    res.status(404).sendFile(path.resolve('views', 'not-found.html'));
+});
+
+export default router;
