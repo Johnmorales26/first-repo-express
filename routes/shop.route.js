f10f6e12765a6a4a57d2107faaaf172ec895a3fb
Jonatan Morales <johnta2610@hotmail.com>
2023-05-22 20:39:16 -0600

    ✨S04A03 Creando Vistas

--- routes/shop.route.js
+++ routes/shop.route.js
@@ -1,24 +1,22 @@
 // Importando el enrutador de express
 import { Router } from 'express';
+//  Importando el gestor de rutas
+import path from 'path';
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
 // GET /
-router.get('/', (req, res)=>{
-    console.log("📢 Sirviendo la ruta '/'");
-    // Se contesta al server
-    res.send(`
-    <h1>Welcome to Express Js</h1>
-    <p>This is my awesome app! 😎</p>
-  `);
+router.get('/', (req, res) => {
+  console.log("📢 Sirviendo la ruta '/'");
+  res.sendFile(path.resolve('views', 'shop.html'));
 });
 
 // GET /about
 router.get('/about', (req, res) => {
-    console.log("📢 Sirviendo la ruta '/about'");
-    // Se contesta al server
-    res.send(`
+  console.log("📢 Sirviendo la ruta '/about'");
+  // Se contesta al server
+  res.send(`
     <h1>🪄 About...</h1>
     <p>App for Fullstack Web Dev Course I!</p>
   `);
